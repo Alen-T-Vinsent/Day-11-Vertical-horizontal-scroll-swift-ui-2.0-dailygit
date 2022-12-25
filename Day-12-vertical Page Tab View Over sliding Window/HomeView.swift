1df057d81a63602143f0c46065ed4de18cc89a5b
Alen T Vinsent <alentvinsent@gmail.com>
2022-12-26 02:01:14 +0530

    with my ui probelem of image is there need to work more just for now pushing

--- Day-12-vertical Page Tab View Over sliding Window/HomeView.swift
+++ Day-12-vertical Page Tab View Over sliding Window/HomeView.swift
@@ -14,53 +14,62 @@ struct HomeView: View {
     init(){
         UIScrollView.appearance().bounces = false
     }
+    
+    @State var currentPage:Int = 2
     var body: some View {
         
         
         ScrollView(.init()){
-            
-            GeometryReader { proxy in
-                let screen = proxy.frame(in: .global)
-                TabView{
-                    ForEach(1...5,id:\.self){index in
-                        
-                        Image("hold on")
-                            .resizable()
-                            .background(Color.red)
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width:getRect().width)
-                            .cornerRadius(1)
-                        //rerotating views for -90 so that it willact as vertical carousel...
-                            //before rotation frame
-                        //just checking
-                            .overlay(
-                                VStack{
-                                    Text("Hello")
-                                    Spacer()
-                                    HStack{
-                                        Text("Hello")
-                                        Spacer()
-                                        Text("Hello")
-                                    }
-                                    Spacer()
-                                    Text("Hello")
-                                })
-                            .frame(width: screen.width,height: screen.height)
-                            .rotationEffect(Angle(degrees: -90))
-                             //after rotation
-                            .frame(width: screen.width,height: screen.height)
-                        
+           
+            TabView{
+                
+                GeometryReader { proxy in
+                    let screen = proxy.frame(in: .global)
+                    
+                    //oversliding animation
+                    let offset = screen.minX
+                    let scale = 1 - (offset/screen.width)
+                    TabView(selection: $currentPage){
+                        ForEach(2...6,id:\.self){index in
+                            
+                            Image("img\(index)")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width:getRect().width).cornerRadius(1)
+                                .modifier(VerticalTabBarViewModifier(screen: screen))
+                                .tag(index)
+                            
+                            
+                            
+//                                .modifier(VerticalTabBarViewModifier(screen: screen))
+
+                                
+                            
+                        }
                     }
+                    //page tabbar
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    
+                    //vertical tabbar#r1
+                    .rotationEffect(.init(degrees: 90))
+                    //adjusting width
+                    .frame(width: screen.width)
+                    
+                    //oversliding effect
+                    .offset(x: -offset)
+                    .scaleEffect(scale >= 0.88 ? scale : 0.88 ,anchor: .center)
+                    .offset(x: -offset)
+                    .blur(radius: (1-scale)*20)
+                
+                    
                 }
-                //page tabbar
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                //vertical tabbar#r1
-                .rotationEffect(.init(degrees: 90))
-                //adjusting width
-                .frame(width: screen.width)
+                
+                DetailView(currentPage: $currentPage)
             }
-            
+            .background(Color.black)
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
        
         .ignoresSafeArea()
     }
@@ -80,3 +89,46 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
     }
 }
+
+struct VerticalTabBarViewModifier:ViewModifier{
+    var screen:CGRect
+    func body(content: Content) -> some View {
+        return content
+        //rerotating views for -90 so that it willact as vertical carousel...
+            //before rotation frame
+            .frame(width: screen.width,height: screen.height)
+            .rotationEffect(Angle(degrees: -90))
+             //after rotation
+            .frame(width: screen.height,height: screen.width)
+        
+        
+    }
+}
+
+struct DetailView:View{
+    @Binding var currentPage:Int
+    var colors:[Color] = [.red, .gray, .green, .yellow, .blue,.white,.orange,.pink]
+    
+    var body:some View{
+        VStack{
+            Text("Details")
+                .font(.title.bold())
+                .frame(maxWidth: .infinity,alignment: .leading)
+                .padding(20)
+            Image("img\(currentPage)")
+                .resizable()
+                
+                .frame(width: 250,height: 250)
+                .aspectRatio(contentMode: .fit)
+                .padding()
+                
+            
+            Spacer()
+        }
+        .background(colors[currentPage].blur(radius: 70))
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
+    }
+}
+
+//edges
+var edges = UIApplication.shared.windows.first?.safeAreaInsets
